1a14d41af43a1a779f20f21afb7a293c1d1bca34
TiveCS <ahmadfathanhanif@gmail.com>
2023-01-19 17:44:27 +0700

    add forgotten test for threads plugin

---
+++ src/Infrastructures/http/_test/threads.test.js
@@ -0,0 +1,70 @@
+const pool = require('../../database/postgres/pool');
+const ThreadsTableTestHelper = require('../../../../tests/ThreadsTableTestHelper');
+const container = require('../../container');
+const createServer = require('../createServer');
+const AuthenticationTokenManager = require('../../../Applications/security/AuthenticationTokenManager');
+
+describe('/threads endpoints', () => {
+  afterAll(async () => {
+    await pool.end();
+  });
+
+  afterEach(async () => {
+    await ThreadsTableTestHelper.cleanTable();
+  });
+
+  describe('when POST /threads', () => {
+    it('should response 401 when request not contain Authorization header', async () => {
+      // Arrange
+      const requestPayload = {
+        title: 'Dicoding Indonesia',
+        body: 'Dicoding Indonesia',
+      };
+      const server = await createServer(container);
+
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/threads',
+        payload: requestPayload,
+      });
+
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(401);
+      expect(responseJson.error).toEqual('Unauthorized');
+      expect(responseJson.message).toEqual('Missing authentication');
+    });
+
+    it('should response 201 and persisted thread', async () => {
+      // Arrange
+      const requestPayload = {
+        title: 'Dicoding Indonesia',
+        body: 'Dicoding Indonesia',
+      };
+      const server = await createServer(container);
+      const tokenManager = container.getInstance(
+        AuthenticationTokenManager.name,
+      );
+      const accessToken = await tokenManager.createAccessToken({
+        id: 'user-123',
+      });
+
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/threads',
+        payload: requestPayload,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(201);
+      expect(responseJson.status).toEqual('success');
+      expect(responseJson.data.addedThread).toBeDefined();
+    });
+  });
+});
