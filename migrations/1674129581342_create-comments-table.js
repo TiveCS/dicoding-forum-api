3ebe97d6f225e1ac5475550f536355cc67a30af7
TiveCS <ahmadfathanhanif@gmail.com>
2023-01-20 01:46:06 +0700

    implement comments migration

--- migrations/1674129581342_create-comments-table.js
+++ migrations/1674129581342_create-comments-table.js
@@ -3,7 +3,32 @@
 exports.shorthands = undefined;
 
 exports.up = (pgm) => {
-  pgm.createTable('comments', {});
+  pgm.createTable('comments', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    thread_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      foreignKeys: {
+        references: 'threads(id)',
+        onDelete: 'cascade',
+      },
+    },
+    content: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    owner: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      foreignKeys: {
+        references: 'users(id)',
+        onDelete: 'cascade',
+      },
+    },
+  });
 };
 
 exports.down = (pgm) => {
