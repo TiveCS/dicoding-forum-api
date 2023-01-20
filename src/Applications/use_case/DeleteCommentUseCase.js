5e3d6cb287fc3326d0d85a1634680809bccc44e6
TiveCS <ahmadfathanhanif@gmail.com>
2023-01-20 16:40:16 +0700

    add DeleteCommentUseCase

---
+++ src/Applications/use_case/DeleteCommentUseCase.js
@@ -0,0 +1,19 @@
+class DeleteCommentUseCase {
+  constructor({ commentRepository }) {
+    this._commentRepository = commentRepository;
+  }
+
+  async execute(useCasePayload, owner) {
+    const { commentId } = useCasePayload;
+
+    await this_._commentRepository.verifyCommentOwner(commentId, owner);
+
+    const deletedComment = await this._commentRepository.deleteCommentById(
+      commentId,
+    );
+
+    return deletedComment;
+  }
+}
+
+module.exports = DeleteCommentUseCase;
---
+++ src/Applications/use_case/_test/DeleteCommentUseCase.test.js
@@ -0,0 +1,38 @@
+const DeleteCommentUseCase = require('../DeleteCommentUseCase');
+const CommentRepository = require('../../../Domains/comments/CommentRepository');
+
+describe('DeleteCommentUseCase', () => {
+  it('should orchestrating the delete comment action correctly', async () => {
+    // Arrange
+    const useCasePayload = {
+      commentId: 'comment-123',
+    };
+    const owner = 'user-123';
+
+    const expectedDeletedComment = {
+      id: 'comment-123',
+    };
+
+    const mockCommentRepository = new CommentRepository();
+
+    mockCommentRepository.deleteCommentById = jest
+      .fn()
+      .mockImplementation(() => Promise.resolve(expectedDeletedComment));
+
+    const deleteCommentUseCase = new DeleteCommentUseCase({
+      commentRepository: mockCommentRepository,
+    });
+
+    // Action
+    const deletedComment = await deleteCommentUseCase.execute(
+      useCasePayload,
+      owner,
+    );
+
+    // Assert
+    expect(deletedComment).toStrictEqual(expectedDeletedComment);
+    expect(mockCommentRepository.deleteCommentById).toBeCalledWith(
+      useCasePayload.commentId,
+    );
+  });
+});
