6616236f741270193ab9c295a677362fdb613216
TiveCS <ahmadfathanhanif@gmail.com>
2023-01-21 17:50:24 +0700

    implement addComment interface api

---
+++ src/Infrastructures/http/_test/comments.test.js
@@ -0,0 +1,114 @@
+const createServer = require('../createServer');
+const container = require('../../container');
+const pool = require('../../database/postgres/pool');
+const CommentsTableTestHelper = require('../../../../tests/CommentsTableTestHelper');
+const ThreadsTableTestHelper = require('../../../../tests/ThreadsTableTestHelper');
+const AuthenticationTokenManager = require('../../../Applications/security/AuthenticationTokenManager');
+
+describe('/threads/{threadId}/comments endpoint', () => {
+  afterAll(async () => {
+    await pool.end();
+  });
+
+  afterEach(async () => {
+    await CommentsTableTestHelper.cleanTable();
+    await ThreadsTableTestHelper.cleanTable();
+  });
+
+  describe('when POST /threads/{threadId}/comments', () => {
+    it('should response 201 and persisted comment', async () => {
+      // Arrange
+      const requestPayload = {
+        content: 'sebuah comment',
+      };
+      const owner = 'user-123';
+
+      await ThreadsTableTestHelper.addThread({
+        id: 'thread-123',
+        body: 'ini body',
+        title: 'ini title',
+        owner,
+      });
+
+      const expectedResponsePayload = {
+        id: 'comment-123',
+        content: requestPayload.content,
+        owner,
+      };
+
+      const server = await createServer(container);
+      const tokenManager = container.getInstance(
+        AuthenticationTokenManager.name,
+      );
+      const accessToken = await tokenManager.createAccessToken({ id: owner });
+
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/threads/thread-123/comments',
+        payload: requestPayload,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(201);
+      expect(responseJson.status).toEqual('success');
+      expect(responseJson.data.addedComment).toBeDefined();
+    });
+
+    it('should response 401 when request not contain Authorization', async () => {
+      // Arrange
+      const requestPayload = {
+        content: 'sebuah comment',
+      };
+
+      const server = await createServer(container);
+
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/threads/thread-123/comments',
+        payload: requestPayload,
+      });
+
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(401);
+      expect(responseJson.error).toEqual('Unauthorized');
+      expect(responseJson.message).toEqual('Missing authentication');
+    });
+
+    it('should response 404 when thread not found', async () => {
+      // Arrange
+      const requestPayload = {
+        content: 'sebuah comment',
+      };
+      const owner = 'user-123';
+
+      const server = await createServer(container);
+      const tokenManager = container.getInstance(
+        AuthenticationTokenManager.name,
+      );
+      const accessToken = await tokenManager.createAccessToken({ id: owner });
+
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/threads/thread-123/comments',
+        payload: requestPayload,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(404);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual('Thread tidak ditemukan');
+    });
+  });
+});
---
+++ src/Interfaces/http/api/comments/handler.js
@@ -0,0 +1,39 @@
+const AddCommentUseCase = require('../../../../Applications/use_case/AddCommentUseCase');
+const DeleteCommentUseCase = require('../../../../Applications/use_case/DeleteCommentUseCase');
+
+class CommentsHandler {
+  constructor(container) {
+    this._container = container;
+
+    this.postCommentHandler = this.postCommentHandler.bind(this);
+    this.deleteCommentHandler = this.deleteCommentHandler.bind(this);
+  }
+
+  async postCommentHandler(request, h) {
+    const addCommentUseCase = this._container.getInstance(
+      AddCommentUseCase.name,
+    );
+
+    const owner = request.auth.credentials.id;
+    const useCasePayload = {
+      content: request.payload.content,
+      threadId: request.params.threadId,
+    };
+
+    const addedComment = await addCommentUseCase.execute(useCasePayload, owner);
+
+    const response = h.response({
+      status: 'success',
+      message: 'Comment berhasil ditambahkan',
+      data: {
+        addedComment,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+
+  async deleteCommentHandler(request, h) {}
+}
+
+module.exports = CommentsHandler;
